50ab6f50a8c2536958068c99a2bcef96a4ede0df
Felix Angelov <felangelov@gmail.com>
2020-03-11 22:17:22 -0500

    Add Player Prefab + Script

---
+++ Assets/Resources/Prefabs/Player/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float speed = 20f;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        move(new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+    }
+
+    void move(Vector2 direction)
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
